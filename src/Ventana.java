d68b6f834a905df73c83419273376d45546dc5d2
JoseLuisGL <89991775+JoseLuisGL@users.noreply.github.com>
2023-05-27 19:01:37 -0700

    Panel crear alumno y docente

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,4 +1,5 @@
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -7,6 +8,7 @@ import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.SwingConstants;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
@@ -99,7 +101,7 @@ public class Ventana extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				remove(fondo);
 				anterior = actual;
-				actual = "registro";
+				actual = "menu";
 				add(menu());
 				repaint();
 				revalidate();
@@ -220,6 +222,12 @@ public class Ventana extends JFrame{
 		btnCrearAlu.setBackground(new Color(121, 255, 145));
 		btnCrearAlu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				remove(fondo);
+				anterior = actual;
+				actual = "crearAlumno";
+				add(crearAlumno());
+				repaint();
+				revalidate();
 			}
 		});
 		btnCrearAlu.setFont(new Font("Tahoma", Font.BOLD, 11));
@@ -228,6 +236,16 @@ public class Ventana extends JFrame{
 		
 		JButton btnCrearDoce = new JButton("Crear");
 		btnCrearDoce.setBackground(new Color(121, 255, 145));
+		btnCrearDoce.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				remove(fondo);
+				anterior = actual;
+				actual = "crearDocente";
+				add(crearDocente());
+				repaint();
+				revalidate();
+			}
+		});
 		btnCrearDoce.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnCrearDoce.setBounds(176, 366, 90, 40);
 		fondo.add(btnCrearDoce);
@@ -318,6 +336,16 @@ public class Ventana extends JFrame{
 		btnCerrarSesion.setForeground(new Color(255, 255, 255));
 		btnCerrarSesion.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnCerrarSesion.setBackground(new Color(255, 0, 0));
+		btnCerrarSesion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				remove(fondo);
+				anterior = actual;
+				actual = "login";
+				add(login());
+				repaint();
+				revalidate();
+			}
+		});
 		btnCerrarSesion.setBounds(410, 496, 125, 40);
 		fondo.add(btnCerrarSesion);
 		
@@ -330,6 +358,373 @@ public class Ventana extends JFrame{
 		return fondo;
 	}
 	
+	public JPanel crearAlumno() {
+		anterior = actual;
+		actual = "crearAlumno";
+		JPanel fondo = new JPanel();
+		fondo.setBackground(new Color(49, 64, 81));
+		fondo.setBounds(0, 0, 584, 561);
+		getContentPane().add(fondo);
+		fondo.setLayout(null);
+		
+		JLabel Titulo = new JLabel("Alumno - Crear");
+		Titulo.setForeground(new Color(0, 0, 0));
+		Titulo.setHorizontalAlignment(SwingConstants.CENTER);
+		Titulo.setFont(new Font("Tahoma", Font.PLAIN, 35));
+		Titulo.setBounds(79, 11, 421, 40);
+		fondo.add(Titulo);
+		
+		JLabel Titulo2 = new JLabel("Obligatorio: Llene todos los campos");
+		Titulo2.setForeground(new Color(255, 0, 0));
+		Titulo2.setHorizontalAlignment(SwingConstants.CENTER);
+		Titulo2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		Titulo2.setBounds(79, 50, 421, 25);
+		fondo.add(Titulo2);
+		
+		JPanel fondo2 = new JPanel();
+		fondo2.setBackground(new Color(0, 128, 64));
+		fondo2.setBounds(60, 86, 480, 405);
+		fondo.add(fondo2);
+		fondo2.setLayout(null);
+		
+		JLabel tag1 = new JLabel("Nombres:");
+		tag1.setBounds(25, 11, 90, 25);
+		fondo2.add(tag1);
+		tag1.setBackground(new Color(255, 255, 255));
+		tag1.setForeground(new Color(255, 255, 255));
+		tag1.setHorizontalAlignment(SwingConstants.LEFT);
+		tag1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel tag2 = new JLabel("Apellidos:");
+		tag2.setBounds(25, 62, 90, 25);
+		fondo2.add(tag2);
+		tag2.setHorizontalAlignment(SwingConstants.LEFT);
+		tag2.setForeground(new Color(255, 255, 255));
+		tag2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag2.setBackground(Color.WHITE);
+		
+		JLabel tag3 = new JLabel("Fecha de nacimiento:");
+		tag3.setBounds(25, 112, 169, 25);
+		fondo2.add(tag3);
+		tag3.setHorizontalAlignment(SwingConstants.LEFT);
+		tag3.setForeground(new Color(255, 255, 255));
+		tag3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag3.setBackground(Color.WHITE);
+		
+		JLabel tag4 = new JLabel("Correo electronico:");
+		tag4.setBounds(25, 170, 169, 25);
+		fondo2.add(tag4);
+		tag4.setHorizontalAlignment(SwingConstants.LEFT);
+		tag4.setForeground(new Color(255, 255, 255));
+		tag4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag4.setBackground(Color.WHITE);
+		
+		JLabel tag7 = new JLabel("Grado:");
+		tag7.setBounds(25, 333, 90, 25);
+		fondo2.add(tag7);
+		tag7.setHorizontalAlignment(SwingConstants.LEFT);
+		tag7.setForeground(new Color(255, 255, 255));
+		tag7.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag7.setBackground(Color.WHITE);
+		
+		JLabel tag5 = new JLabel("Teléfono:");
+		tag5.setBounds(25, 221, 90, 25);
+		fondo2.add(tag5);
+		tag5.setHorizontalAlignment(SwingConstants.LEFT);
+		tag5.setForeground(new Color(255, 255, 255));
+		tag5.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag5.setBackground(Color.WHITE);
+		
+		JLabel tag6 = new JLabel("Foto:");
+		tag6.setBounds(25, 277, 90, 25);
+		fondo2.add(tag6);
+		tag6.setHorizontalAlignment(SwingConstants.LEFT);
+		tag6.setForeground(new Color(255, 255, 255));
+		tag6.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag6.setBackground(Color.WHITE);
+		
+		JTextField nombres = new JTextField();
+		nombres.setBackground(new Color(0, 128, 192));
+		nombres.setBounds(25, 36, 420, 25);
+		fondo2.add(nombres);
+		nombres.setColumns(10);
+		
+		JTextField apellidos = new JTextField();
+		apellidos.setColumns(10);
+		apellidos.setBackground(new Color(0, 128, 192));
+		apellidos.setBounds(25, 86, 420, 25);
+		fondo2.add(apellidos);
+		
+		JTextField fechaNacimiento = new JTextField();
+		fechaNacimiento.setColumns(10);
+		fechaNacimiento.setBackground(new Color(0, 128, 192));
+		fechaNacimiento.setBounds(25, 140, 420, 25);
+		fondo2.add(fechaNacimiento);
+		
+		JTextField correo = new JTextField();
+		correo.setColumns(10);
+		correo.setBackground(new Color(0, 128, 192));
+		correo.setBounds(25, 195, 420, 25);
+		fondo2.add(correo);
+		
+		JTextField telefono = new JTextField();
+		telefono.setColumns(10);
+		telefono.setBackground(new Color(0, 128, 192));
+		telefono.setBounds(25, 246, 420, 25);
+		fondo2.add(telefono);
+		
+		JTextField grado = new JTextField();
+		grado.setColumns(10);
+		grado.setBackground(new Color(0, 128, 192));
+		grado.setBounds(25, 356, 420, 25);
+		fondo2.add(grado);
+		
+		JButton archivo = new JButton("Subir archivo");
+		archivo.setBackground(new Color(192, 192, 192));
+		archivo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String Ruta = "";
+		        JFileChooser jFileChooser = new JFileChooser();
+		        FileNameExtensionFilter filtrado = new FileNameExtensionFilter("JGP, PNG & GIF", "jpg", "png", "gif");
+		        jFileChooser.setFileFilter(filtrado);
+		        
+		        int respuesta = jFileChooser.showOpenDialog(fondo);
+		        
+		        if (respuesta == JFileChooser.APPROVE_OPTION) {
+		            Ruta = jFileChooser.getSelectedFile().getPath();
+		            
+		            JLabel lblImagen = new JLabel("");
+		            Image mImagen = new ImageIcon(Ruta).getImage();
+		            ImageIcon mIcono = new ImageIcon(mImagen.getScaledInstance(200, 200, Image.SCALE_DEFAULT));
+		            lblImagen.setIcon(mIcono);
+		            lblImagen.setBounds(200, 200, 200, 200);
+		    		fondo2.add(lblImagen);
+		            
+		        }
+			}
+		});
+		archivo.setBounds(26, 303, 116, 23);
+		fondo2.add(archivo);
+		
+		JButton Volver = new JButton("Volver");
+		Volver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				remove(fondo);
+				anterior = actual;
+				actual = "menu";
+				add(menu());
+				repaint();
+				revalidate();
+			}
+		});
+		Volver.setForeground(new Color(255, 255, 255));
+		Volver.setBackground(new Color(255, 0, 0));
+		Volver.setBounds(173, 514, 89, 23);
+		fondo.add(Volver);
+		
+		JButton Crear = new JButton("Crear");
+		Crear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		Crear.setForeground(new Color(255, 255, 255));
+		Crear.setBackground(new Color(0, 128, 255));
+		Crear.setBounds(342, 514, 89, 23);
+		fondo.add(Crear);
+
+		JLabel imagen = new JLabel("");
+		ImageIcon imageIcon = new ImageIcon(new ImageIcon("img/crear.png").getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT));
+		imagen.setIcon(imageIcon);
+		imagen.setBounds(450, 5, 80, 80);
+		fondo.add(imagen);
+		
+		this.add(fondo);
+		return fondo;
+	}
+	
+	public JPanel crearDocente() {
+		anterior = actual;
+		actual = "crearAlumno";
+		JPanel fondo = new JPanel();
+		fondo.setBackground(new Color(49, 64, 81));
+		fondo.setBounds(0, 0, 584, 561);
+		getContentPane().add(fondo);
+		fondo.setLayout(null);
+		
+		JLabel Titulo = new JLabel("Docente - Crear");
+		Titulo.setForeground(new Color(0, 0, 0));
+		Titulo.setHorizontalAlignment(SwingConstants.CENTER);
+		Titulo.setFont(new Font("Tahoma", Font.PLAIN, 35));
+		Titulo.setBounds(79, 11, 421, 40);
+		fondo.add(Titulo);
+		
+		JLabel Titulo2 = new JLabel("Obligatorio: Llene todos los campos");
+		Titulo2.setForeground(new Color(255, 0, 0));
+		Titulo2.setHorizontalAlignment(SwingConstants.CENTER);
+		Titulo2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		Titulo2.setBounds(79, 50, 421, 25);
+		fondo.add(Titulo2);
+		
+		JPanel fondo2 = new JPanel();
+		fondo2.setBackground(new Color(0, 128, 64));
+		fondo2.setBounds(60, 86, 480, 405);
+		fondo.add(fondo2);
+		fondo2.setLayout(null);
+		
+		JLabel tag1 = new JLabel("Nombres:");
+		tag1.setBounds(25, 11, 90, 25);
+		fondo2.add(tag1);
+		tag1.setBackground(new Color(255, 255, 255));
+		tag1.setForeground(new Color(255, 255, 255));
+		tag1.setHorizontalAlignment(SwingConstants.LEFT);
+		tag1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel tag2 = new JLabel("Apellidos:");
+		tag2.setBounds(25, 62, 90, 25);
+		fondo2.add(tag2);
+		tag2.setHorizontalAlignment(SwingConstants.LEFT);
+		tag2.setForeground(new Color(255, 255, 255));
+		tag2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag2.setBackground(Color.WHITE);
+		
+		JLabel tag3 = new JLabel("Fecha de nacimiento:");
+		tag3.setBounds(25, 112, 169, 25);
+		fondo2.add(tag3);
+		tag3.setHorizontalAlignment(SwingConstants.LEFT);
+		tag3.setForeground(new Color(255, 255, 255));
+		tag3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag3.setBackground(Color.WHITE);
+		
+		JLabel tag4 = new JLabel("Correo electronico:");
+		tag4.setBounds(25, 170, 169, 25);
+		fondo2.add(tag4);
+		tag4.setHorizontalAlignment(SwingConstants.LEFT);
+		tag4.setForeground(new Color(255, 255, 255));
+		tag4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag4.setBackground(Color.WHITE);
+		
+		JLabel tag7 = new JLabel("Grado de estudios:");
+		tag7.setBounds(25, 333, 150, 25);
+		fondo2.add(tag7);
+		tag7.setHorizontalAlignment(SwingConstants.LEFT);
+		tag7.setForeground(new Color(255, 255, 255));
+		tag7.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag7.setBackground(Color.WHITE);
+		
+		JLabel tag5 = new JLabel("Teléfono:");
+		tag5.setBounds(25, 221, 90, 25);
+		fondo2.add(tag5);
+		tag5.setHorizontalAlignment(SwingConstants.LEFT);
+		tag5.setForeground(new Color(255, 255, 255));
+		tag5.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag5.setBackground(Color.WHITE);
+		
+		JLabel tag6 = new JLabel("Foto:");
+		tag6.setBounds(25, 277, 90, 25);
+		fondo2.add(tag6);
+		tag6.setHorizontalAlignment(SwingConstants.LEFT);
+		tag6.setForeground(new Color(255, 255, 255));
+		tag6.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		tag6.setBackground(Color.WHITE);
+		
+		JTextField nombres = new JTextField();
+		nombres.setBackground(new Color(0, 128, 192));
+		nombres.setBounds(25, 36, 420, 25);
+		fondo2.add(nombres);
+		nombres.setColumns(10);
+		
+		JTextField apellidos = new JTextField();
+		apellidos.setColumns(10);
+		apellidos.setBackground(new Color(0, 128, 192));
+		apellidos.setBounds(25, 86, 420, 25);
+		fondo2.add(apellidos);
+		
+		JTextField fechaNacimiento = new JTextField();
+		fechaNacimiento.setColumns(10);
+		fechaNacimiento.setBackground(new Color(0, 128, 192));
+		fechaNacimiento.setBounds(25, 140, 420, 25);
+		fondo2.add(fechaNacimiento);
+		
+		JTextField correo = new JTextField();
+		correo.setColumns(10);
+		correo.setBackground(new Color(0, 128, 192));
+		correo.setBounds(25, 195, 420, 25);
+		fondo2.add(correo);
+		
+		JTextField telefono = new JTextField();
+		telefono.setColumns(10);
+		telefono.setBackground(new Color(0, 128, 192));
+		telefono.setBounds(25, 246, 420, 25);
+		fondo2.add(telefono);
+		
+		JTextField grado = new JTextField();
+		grado.setColumns(10);
+		grado.setBackground(new Color(0, 128, 192));
+		grado.setBounds(25, 356, 420, 25);
+		fondo2.add(grado);
+		
+		JButton archivo = new JButton("Subir archivo");
+		archivo.setBackground(new Color(192, 192, 192));
+		archivo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String Ruta = "";
+		        JFileChooser jFileChooser = new JFileChooser();
+		        FileNameExtensionFilter filtrado = new FileNameExtensionFilter("JGP, PNG & GIF", "jpg", "png", "gif");
+		        jFileChooser.setFileFilter(filtrado);
+		        
+		        int respuesta = jFileChooser.showOpenDialog(fondo);
+		        
+		        if (respuesta == JFileChooser.APPROVE_OPTION) {
+		            Ruta = jFileChooser.getSelectedFile().getPath();
+		            
+		            JLabel lblImagen = new JLabel("");
+		            Image mImagen = new ImageIcon(Ruta).getImage();
+		            ImageIcon mIcono = new ImageIcon(mImagen.getScaledInstance(200, 200, Image.SCALE_DEFAULT));
+		            lblImagen.setIcon(mIcono);
+		            lblImagen.setBounds(200, 200, 200, 200);
+		    		fondo2.add(lblImagen);
+		            
+		        }
+			}
+		});
+		archivo.setBounds(26, 303, 116, 23);
+		fondo2.add(archivo);
+		
+		JButton Volver = new JButton("Volver");
+		Volver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				remove(fondo);
+				anterior = actual;
+				actual = "menu";
+				add(menu());
+				repaint();
+				revalidate();
+			}
+		});
+		Volver.setForeground(new Color(255, 255, 255));
+		Volver.setBackground(new Color(255, 0, 0));
+		Volver.setBounds(173, 514, 89, 23);
+		fondo.add(Volver);
+		
+		JButton Crear = new JButton("Crear");
+		Crear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		Crear.setForeground(new Color(255, 255, 255));
+		Crear.setBackground(new Color(0, 128, 255));
+		Crear.setBounds(342, 514, 89, 23);
+		fondo.add(Crear);
+
+		JLabel imagen = new JLabel("");
+		ImageIcon imageIcon = new ImageIcon(new ImageIcon("img/crear.png").getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT));
+		imagen.setIcon(imageIcon);
+		imagen.setBounds(450, 5, 80, 80);
+		fondo.add(imagen);
+		
+		this.add(fondo);
+		return fondo;
+	}
 	
 	public void limpiarVentana() {
 		
@@ -339,6 +734,30 @@ public class Ventana extends JFrame{
 		
 		if(actual.equals("login")){
 			panel = login(); 
+			
+			this.add(panel);
+			
+			this.repaint();
+			this.revalidate();
+		}
+		if(actual.equals("menu")){
+			panel = menu(); 
+			
+			this.add(panel);
+			
+			this.repaint();
+			this.revalidate();
+		}
+		if(actual.equals("crearAlumno")){
+			panel = crearAlumno(); 
+			
+			this.add(panel);
+			
+			this.repaint();
+			this.revalidate();
+		}
+		if(actual.equals("crearDocente")){
+			panel = crearDocente(); 
 			
 			this.add(panel);
 			
